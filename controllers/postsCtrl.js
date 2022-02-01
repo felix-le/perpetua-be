14913655cccd9ce322c41b2d7b5b63c81989f1f6
Viet Anh Le <mc.vietanh@gmail.com>
2022-02-01 10:39:40 -0500

    working on sort

--- controllers/postsCtrl.js
+++ controllers/postsCtrl.js
@@ -8,37 +8,48 @@ const postsCtrl = {
   getAllPosts: async (req, res) => {
     try {
       const { tag } = req.query;
-      const sourceData = await getAllPostsFromServerLink(tag);
-      const posts = sourceData.posts;
 
-      // group all posts by authorId
-      const groupedData = _.groupBy(posts, 'authorId');
-      // merge properties likes, popularity, reads, tags array in the same authorId
-      const mergedData = _.map(groupedData, (value, key) => {
-        const authorId = key;
-        // keep the value of author
-        const author = value[0].author;
+      if (tag) {
+        const sourceData = await getAllPostsFromServerLink(tag);
 
-        const likes = _.sumBy(value, 'likes');
-        const popularity = _.sumBy(value, 'popularity');
-        const reads = _.sumBy(value, 'reads');
-        const tags = _.uniq(_.flatten(_.map(value, 'tags')));
-        return {
-          author,
-          authorId,
-          likes,
-          popularity,
-          reads,
-          tags,
-        };
-      });
+        const posts = sourceData.posts;
 
-      return responseServer(
-        res,
-        statusConstants.SUCCESS_CODE,
-        'Get all Posts successfully',
-        mergedData
-      );
+        // group all posts by authorId
+        const groupedData = _.groupBy(posts, 'authorId');
+        // merge properties likes, popularity, reads, tags array in the same authorId
+        const mergedData = _.map(groupedData, (value, key) => {
+          const authorId = key;
+          // keep the value of author
+          const author = value[0].author;
+          const id = value[0].id;
+          const likes = _.sumBy(value, 'likes');
+          const popularity = _.sumBy(value, 'popularity');
+          const reads = _.sumBy(value, 'reads');
+          const tags = _.uniq(_.flatten(_.map(value, 'tags')));
+          return {
+            id,
+            author,
+            authorId,
+            likes,
+            popularity,
+            reads,
+            tags,
+          };
+        });
+
+        return responseServer(
+          res,
+          statusConstants.SUCCESS_CODE,
+          'Get all Posts successfully',
+          mergedData
+        );
+      } else {
+        return raiseException(
+          res,
+          statusConstants.BAD_REQUEST_CODE,
+          'The tag parameter is required'
+        );
+      }
     } catch (error) {
       logger.error(error);
       return raiseException(
